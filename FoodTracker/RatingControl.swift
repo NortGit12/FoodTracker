824e7acddb97f809edf5afff548590b5b344caf3
Jeff Norton <jeff12ntn@gmail.com>
2016-06-22 16:28:09 -0600

    Completed the "Implement a Custom Control" section

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,97 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Jeff Norton on 6/22/16.
+//  Copyright © 2016 JCN. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+    
+    // MARK: Properties
+    
+    var rating = 0 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+    var ratingButtons = [UIButton]()
+    
+    let starCount = 5
+    let spacing = 5
+
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let emptyStarImage = UIImage(named: "emptyStar")
+        let filledStarImage = UIImage(named: "filledStar")
+        
+        for _ in 0..<starCount {
+            
+            let button = UIButton()
+            
+            button.setImage(emptyStarImage, forState: .Normal)
+            button.setImage(filledStarImage, forState: .Selected)
+            button.setImage(filledStarImage, forState: [.Highlighted, .Selected])
+            
+            button.adjustsImageWhenHighlighted = false
+            
+            // Map the .TouchDown event to the ratingButtonTapped(_:) function
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            
+            ratingButtons += [button]
+            
+            addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        
+        // Set thebutton's width and height to a square the size of the frame's height
+        let buttonSize = Int(frame.size.height)
+        
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: 44)
+        
+        // Offset each button's origin by the length of the button plus spacing
+        for (index, button) in ratingButtons.enumerate() {
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
+            button.frame = buttonFrame
+        }
+        
+        updateButtonSelectionStates()
+        
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
+    }
+    
+    // MARK: Button action
+    
+    func ratingButtonTapped(button: UIButton) {
+        rating = ratingButtons.indexOf(button)! + 1
+        
+        updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        
+        for (index, button) in ratingButtons.enumerate() {
+            /*
+             If the index of a button is less than the rating, that button should be selected.
+             For example, if the rating is 3, the buttons at index 0, 1, & 2 are selected.
+             */
+            button.selected = index < rating
+        }
+        
+    }
+    
+}
--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var mealNameLabel: UILabel!
     
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -80,11 +81,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
         presentViewController(imagePickerController, animated: true, completion: nil)
     }
-
-    @IBAction func setDefaultLabelText(sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
 
 }
 
